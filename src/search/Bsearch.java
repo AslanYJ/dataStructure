976046324800c7375f0c68784cc24267907c8107
ASlanYJ <121601585@qq.com>
2019-01-13 21:22:59 +0800

    二分查找变种题

--- src/search/Bsearch.java
+++ src/search/Bsearch.java
@@ -6,9 +6,12 @@ package search;
 public class Bsearch {
 
     public static void main(String[] args) {
-        int[] a = new int[] {1,2,3,4,5};
+//        int[] a = new int[] {1,2,3,4,5};
+//        int value = 5;
+//        System.out.println(new Bsearch().bsearch(a,value,a.length));
+        int[] a = new int[] {1,3,4,5,6,8,8,8,11,18};
         int value = 5;
-        System.out.println(new Bsearch().bsearch(a,value,a.length));
+        System.out.println(new Bsearch().bsearch4(a,value,a.length));
     }
 
     /**
@@ -29,6 +32,101 @@ public class Bsearch {
                 low = mid + 1;//如果mid的值比value小，证明在右边
             } else if(array[mid] > num) {
                 high = mid - 1;//如果mid的值比value小，证明在左边
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个值等于给定的值
+     * @param a 有序数组
+     * @param value 要查找的值
+     * @param n 数组的长度
+     * @return 数组中第一个与给定的值相等的下标
+     */
+    public int beserch1(int[] a,int value,int n) {
+        int low = 0;
+        int high = n - 1;//数组最后的下标
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if(a[mid] < value) {
+                low = mid + 1;
+            } else if(a[mid] > value) {
+                high = mid - 1;
+            } else if(a[mid] == value) {
+                if(mid ==0 || a[mid - 1] != value) return mid;
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 查找最后一个值等于给定的值
+     * @param a 有序数组
+     * @param value 要查找的值
+     * @param n 数组的长度
+     * @return 数组中第一个与给定的值相等的下标
+     */
+    public int bsearch2(int[] a, int value ,int n) {
+        int low = 0;
+        int high = n - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if(a[mid] < value) {
+                low = mid + 1;
+            } else if(a[mid] > value) {
+                high = mid - 1;
+            } else if(a[mid] == value) {
+                if(mid == n - 1 || a[mid+1] != value) return mid;
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 查找第一个大于或者等于给定的值
+     * @param a 有序数组
+     * @param value 要查找的值
+     * @param n 数组的长度
+     * @return 数组中第一个大于或者等于给定的值的下标
+     */
+    public int bsearch3(int[] a,int value,int n) {
+        //两种情况，小于，或者大于等于
+        int low = 0;
+        int high = n - 1;
+        while(low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if(a[mid] >= value) {
+                if(mid ==0 || a[mid - 1] < value) return mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于或者等于给定的值
+     * @param a 有序数组
+     * @param value 要查找的值
+     * @param n 数组的长度
+     * @return 数组中最后一个小于或者等于给定的值的下标
+     */
+    public int bsearch4(int[] a,int value,int n) {
+        int low = 0;
+        int high = n-1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if(a[mid] <= value) {
+                if(mid == n-1 || a[mid + 1] > value) return mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,12 @@
 package test;
 
 public class Test {
+
     public static void main(String[] args) {
         Integer a = 100,b=100,c=150,d=150;
         System.out.println(a==b);
         System.out.println(c==d);
     }
+
 }
 
